fee41368a01137a228e330442747b65e83eab8b2
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2017-09-25 19:44:09 -0400

    more cleanup, additional states

--- src/main/java/org/quuux/opengl/renderer/states/BindVertex.java
+++ src/main/java/org/quuux/opengl/renderer/states/BindVertex.java
@@ -1,11 +1,11 @@
-package org.quuux.opengl.renderer.commands;
+package org.quuux.opengl.renderer.states;
 
 import com.jogamp.opengl.GL;
 import org.quuux.opengl.lib.VAO;
 import org.quuux.opengl.lib.VBO;
-import org.quuux.opengl.renderer.Command;
+import org.quuux.opengl.renderer.State;
 
-public class BindVertex implements Command {
+public class BindVertex extends State {
 
     private final VBO vbo;
     private final VAO vao;
@@ -16,7 +16,13 @@ public class BindVertex implements Command {
     }
 
     @Override
-    public void run(GL gl) {
+    public void clearState(GL gl) {
+        vbo.clear(gl.getGL4());
+        vao.clear(gl.getGL4());
+    }
+
+    @Override
+    public void setState(GL gl) {
         vbo.bind(gl.getGL4());
         vao.bind(gl.getGL4());
     }
---
+++ src/main/java/org/quuux/opengl/renderer/states/UseProgram.java
@@ -0,0 +1,11 @@
+package org.quuux.opengl.renderer.states;
+
+import org.quuux.opengl.lib.ShaderProgram;
+
+public class UseProgram extends Bind {
+
+    public UseProgram(ShaderProgram program) {
+        super(program);
+    }
+
+}

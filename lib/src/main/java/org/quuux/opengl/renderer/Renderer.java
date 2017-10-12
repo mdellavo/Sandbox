d4899edfb39675c69b4d2eab1d6ed3a1b9c7e0d4
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2017-10-11 21:36:45 -0400

    define renderer interface

--- lib/src/main/java/org/quuux/opengl/renderer/Renderer.java
+++ lib/src/main/java/org/quuux/opengl/renderer/Renderer.java
@@ -1,8 +1,23 @@
 package org.quuux.opengl.renderer;
 
-/**
- * Created by marc on 10/11/17.
- */
+import org.quuux.opengl.renderer.commands.BufferData;
+import org.quuux.opengl.renderer.commands.Clear;
+import org.quuux.opengl.renderer.commands.DrawArrays;
+import org.quuux.opengl.renderer.commands.SetUniformMatrix;
+import org.quuux.opengl.renderer.states.ActivateTexture;
+import org.quuux.opengl.renderer.states.BindVertex;
+import org.quuux.opengl.renderer.states.Blend;
+import org.quuux.opengl.renderer.states.Depth;
+import org.quuux.opengl.renderer.states.UseProgram;
 
 public interface Renderer {
+    void run(BufferData command);
+    void run(Clear command);
+    void run(DrawArrays command);
+    void run(SetUniformMatrix command);
+    void run(ActivateTexture command);
+    void run(BindVertex command);
+    void run(Blend command);
+    void run(Depth command);
+    void run(UseProgram command);
 }
